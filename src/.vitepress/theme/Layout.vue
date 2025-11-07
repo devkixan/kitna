189cdb4555f58ad6b4c69d7cce64b52bc51c6fdf
Kishan Sunar <mailkishansunar@gmail.com>
2025-11-08 02:11:22 +0545

    Add Tailwind CSS integration and update theme styles
    - Introduced Tailwind CSS as a dependency in package.json.
    - Created a new package-lock.json to manage dependencies.
    - Updated the theme layout and styles to utilize Tailwind CSS classes for improved design.
    - Reorganized CSS files, moving styles to a dedicated directory and removing the old style.css file.

--- src/.vitepress/theme/Layout.vue
+++ src/.vitepress/theme/Layout.vue
@@ -6,13 +6,13 @@ const { site, frontmatter } = useData()
 </script>
 
 <template>
-  <div v-if="frontmatter.home">
-    <h1>{{ site.title }}</h1>
-    <p>{{ site.description }}</p>
-    <h2>On Progress</h2>
+  <div v-if="frontmatter.home" class="ks:flex ks:flex-col ks:items-center ks:justify-center ks:h-screen">
+    <h1 class="ks:text-4xl ks:font-bold">{{ site.title }}</h1>
+    <p class="ks:text-lg">{{ site.description }}</p>
+    <h2 class="ks:text-2xl ks:font-bold">On Progress</h2>
   </div>
-  <div v-else>
-    <a href="/">Home</a>
+  <div v-else class="ks:flex ks:flex-col ks:items-center ks:justify-center ks:h-screen">
+    <a href="/" class="ks:text-lg">Home</a>
     <Content />
   </div>
 </template>
