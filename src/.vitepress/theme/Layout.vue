1bb8a8a684b9095d7e98883698f1c192e641f0f6
Kishan Sunar <mailkishansunar@gmail.com>
2025-11-08 04:03:45 +0545

    Refactor Layout.vue and enhance styles in style.css
    - Updated Layout.vue to improve layout structure and visual elements, including background and shadow effects.
    - Modified style.css to introduce new color variables and utility classes for better theming and responsiveness.
    - Enhanced existing styles for improved aesthetics and consistency across the application.

--- src/.vitepress/theme/Layout.vue
+++ src/.vitepress/theme/Layout.vue
@@ -6,18 +6,18 @@ const { site, frontmatter } = useData()
 </script>
 
 <template>
-  <div class="ks:bg-zinc-200 ks:h-screen ks:overflow-y-auto ks:text-white">
+  <div class="ks:bg-zinc-200 ks:h-screen ks:overflow-y-auto">
     <section>
       <div class="ks:container ks:mx-auto ks:px-4">
         <div class="ks:flex ks:justify-center">
           <div class="ks:w-lg ks:relative ks:z-0 ks:flex ks:flex-col ks:justify-center">
             <div class="ks:mx-auto ks:h-40 ks:flex ks:items-center ks:justify-center">
-              <span class="ks:w-10 ks:-mr-4 ks:relative ks:-z-10 ks:origin-bottom-left ks:skew-x-12 ks:h-full  ks:bg-linear-to-t ks:from-blue-900 ks:from-5% ks:via-20% ks:via-blue-800 ks:to-100% ks:to-blue-800 ks:block"></span>
-              <span class="ks:w-10 ks:-ml-4 ks:relative ks:z-10 ks:origin-bottom-right ks:-skew-x-12 ks:h-full ks:bg-linear-to-t ks:from-blue-800 ks:from-5% ks:via-20% ks:via-blue-600 ks:to-100% ks:to-blue-700 ks:block ks:rounded-br-sm"></span>
+              <span class="ks:w-10 ks:-mr-4 ks:relative ks:-z-10 ks:origin-bottom-left ks:skew-x-12 ks:h-full  ks:bg-linear-to-t ks:from-gray-900 ks:from-5% ks:via-20% ks:via-gray-800 ks:to-100% ks:to-gray-800 ks:block"></span>
+              <span class="ks:w-10 ks:-ml-4 ks:relative ks:z-10 ks:origin-bottom-right ks:-skew-x-12 ks:h-full ks:bg-linear-to-t ks:from-gray-800 ks:from-5% ks:via-20% ks:via-gray-600 ks:to-100% ks:to-gray-700 ks:block ks:rounded-br-sm"></span>
             </div>
-            <div class="ks:-mt-7 ks:bg-white ks:backdrop-blur-3xl ks:ring ks:ring-zinc-300 ks:rounded-2xl ks:p-4 ks:py-5">
-              <div class="ks:mx-auto ks:mb-20 ks:text-xs ks:text-white ks:bg-zinc-200 ks:ring ks:ring-zinc-300 ks:w-15 ks:h-4 ks:rounded-full ks:block">
-                <span class="ks:w-10 ks:h-[9px] ks:rounded-bl-sm ks:-translate-px ks:mx-auto ks:relative ks:z-10 ks:skew-x-12 ks:bg-linear-to-t ks:from-blue-800 ks:from-5% ks:via-20% ks:via-blue-700 ks:to-100% ks:to-blue-700 ks:block"></span>
+            <div class="ks:-mt-7 ks:shadow-2xl ks:shadow-black/2 ks:bg-white/80 ks:backdrop-blur-2xl ks:ring ks:ring-white ks:rounded-2xl ks:p-4 ks:py-5">
+              <div class="ks:mx-auto ks:mb-20 ks:text-xs ks:bg-slate-200 ks:ring ks:ring-white ks:w-15 ks:h-3 ks:rounded-full ks:block">
+                <span class="ks:w-10 ks:h-[8px] ks:-translate-x-0.5 ks:rounded-bl-sm ks:mx-auto ks:relative ks:z-10 ks:skew-x-12 ks:bg-linear-to-t ks:from-gray-800 ks:from-5% ks:via-20% ks:via-gray-700 ks:to-100% ks:to-gray-700 ks:block"></span>
               </div>
               <h1 class="ks:text-4xl ks:font-bold">{{ site.title }}</h1>
               <p class="ks:text-lg">{{ site.description }}</p>
@@ -28,7 +28,7 @@ const { site, frontmatter } = useData()
       </div>
     </section>
 
-    <section class="ks:bg-zinc-900 ks:text-white ks:mt-10 ks:min-h-screen ks:border-t ks:border-zinc-800">
+    <section class="ks:mt-10 ks:min-h-screen">
       <div class="ks:container ks:mx-auto ks:px-4">
         Hello
       </div>
