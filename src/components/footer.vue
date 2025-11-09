fd0d0e1e0773ab53eaed92437a5a5c13e905d4e9
Kishan Sunar <mailkishansunar@gmail.com>
2025-11-09 23:17:32 +0545

    Enhance footer component styling with new padding classes
    - Added new padding classes (.ks:pt-5 and .ks:pb-10) in style.css for improved spacing.
    - Updated footer.vue to utilize the new padding classes for better layout control.

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="ks:bg-primary-200 ks:py-10 ks:px-4 ks:relative">
+  <footer class="ks:bg-primary-200 ks:pb-10 ks:pt-5 ks:px-4 ks:relative">
     <div class="ks:container ks:mx-auto ks:px-4">
       <p class="ks:text-center ks:text-zinc-500" aria-hidden="true">
         &copy; {{ new Date().getFullYear() }} Kishan Sunar. All rights reserved.
