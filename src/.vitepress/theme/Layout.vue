30eab7c3276d5380cd605315f41005f219e5e8c3
Kishan Sunar <mailkishansunar@gmail.com>
2025-11-11 00:10:29 +0545

    Refactor Google Tag Manager integration in VitePress configuration
    - Moved Google Tag Manager script from Layout.vue to VitePress config for better organization and maintainability.
    - Removed inline script from Layout.vue to streamline the component structure.

--- src/.vitepress/theme/Layout.vue
+++ src/.vitepress/theme/Layout.vue
@@ -12,14 +12,6 @@ const { site, frontmatter } = useData()
 
 <template>
   <div class="ks:h-screen ks:overflow-y-auto ks:motion-safe:scroll-smooth" role="main">
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PHJB2C2D');</script>
-    <!-- End Google Tag Manager -->
-
     <Hero />
     
     <Projects />
