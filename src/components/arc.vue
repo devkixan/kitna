a23d7ababf707a7ccd2ccfc8f0db76b6b37bff92
Kishan Sunar <kishan@thefireflytech.com>
2026-09-12 01:38:09 +0545

    Refactor code structure for improved readability and maintainability

--- src/components/arc.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="ks:arc-wrapper" v-bind="$attrs" arria-hidden="true">
-    <i class="ks:arc-inner"></i>
-  </div>
-</template>``
--- src/components/footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer class="ks:bg-primary-200 ks:pb-10 ks:pt-5 ks:px-4 ks:relative">
-    <div class="ks:container ks:mx-auto ks:px-4">
-      <p class="ks:text-center ks:text-zinc-500" aria-hidden="true">
-        &copy; {{ new Date().getFullYear() }} Kishan Sunar. All rights reserved.
-      </p>
-    </div>
-  </footer>
-</template>
